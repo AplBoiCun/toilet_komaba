dbb9771c3dc779c1c21737c8b46631b6288dd09c
HasegawaRintaro <Rin@HasegawaRintaro-no-MacBook-Pro.local>
2017-12-22 16:27:12 +0900

    move initMap() from home.html to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function initMap() {
+  var place = {lat: 35.6598964, lng: 139.6835137};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: place
+  });
+  var marker = new google.maps.Marker({
+    position: place,
+    map: map
+  });
+}
